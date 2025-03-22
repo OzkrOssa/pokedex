3e4c28a1180e6878c9fb24f4e8ccdfbe84d97c78
Oscar Ossa <oskrlex17@gmail.com>
2025-03-21 22:08:37 -0500

    Initial Commit

---
+++ app/src/main/java/dev/oscarossa/pokedex/ui/theme/Theme.kt
@@ -0,0 +1,48 @@
+package dev.oscarossa.pokedex.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+private val darkColorScheme = darkColorScheme(
+    primary = Color.Yellow,
+    background = Color(0xFF101010),
+    onBackground = Color.White,
+    surface = Color(0xFF303030),
+    onSurface = Color.White
+)
+
+private val lightColorScheme = lightColorScheme(
+    primary = Color.Blue,
+    background = LightBlue,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black
+)
+
+@Composable
+fun PokedexTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        darkColorScheme
+    } else {
+        lightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
